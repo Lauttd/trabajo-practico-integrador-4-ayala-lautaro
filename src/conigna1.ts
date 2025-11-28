c91fbcb5baf000f65e1cdfb3a94dd71df15f5cc8
Lauttd <lautarayala123090@gmail.com>
2025-11-27 21:06:26 -0300

    se creo el archivo ts consigna 1

--- src/conigna1.ts
+++ src/conigna1.ts
@@ -1,3 +1,27 @@
 
 
+const productName: string = "Leche serenito"
+console.log(productName)
+
+const productPrice: number = 300
+console.log(productPrice)
+
+let productId: string | number //union type
+
+productId = 1
+
+console.log(productId)
+
+productId = "001"
+console.log(productId)
+
+interface Product {
+  name: string,
+  price: number
+}
+
+const myProdcut : Product {
+    name: Minecraft,
+    price: number
+}
 
